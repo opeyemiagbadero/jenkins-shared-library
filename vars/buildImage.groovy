eaa7a9df92ce08f1e4c521a9779940a416d9c566
Opeyemi Gbadero <opeyemiagbadero@gmail.com>
2023-11-26 06:42:06 +0000

    initial commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+def call() {
+    echo "Building the docker image ..."
+    withCredentials([usernamePassword(credentialsId:'docker-hub-repo', passwordVariable:'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh """
+                    docker build -t opeyemiagbadero/demo-app:jma-2.0 .
+                    echo "${PASSWORD} | docker login -u ${USERNAME} --password-stdin"
+                    docker push opeyemiagbadero/demo-app:jma-2.0
+                """
+    }
+    }
